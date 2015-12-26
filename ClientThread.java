2135f4bea4f10a8121d37573cd5c3b6cce94b6e0
lihang <ansjmsmw@icloud.com>
2015-12-26 22:28:17 +0800

    添加了界面，修改了一部分逻辑，界面主要参考了chatroom-master的程序。

---
+++ ClientThread.java
@@ -0,0 +1,201 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+ //receive message thread,handling the message from the server
+    class ClientThread extends JFrame implements Runnable
+      { 
+		private static final long serialVersionUID = 1L;
+	private BufferedReader sin; //buffer，IOstream
+        private PrintWriter sout;  
+        private boolean loginornot=false;   //a flag save the status of login or not
+	    //添加的
+       private  static Socket client;
+        private static final int DEFAULT_WIDTH = 400;
+    	private static final int DEFAULT_HEIGHT = 400;
+      JButton sendButton;
+      JButton quitButton;
+      JButton loginButton;
+      JTextArea chatSpace;
+      JTextArea composeMessageSpace;
+      JTextArea userSpace;
+      JPanel composeMessageSpacePanel;
+      JPanel buttonPanel;
+     
+        public  ClientThread() throws IOException 
+	    { 
+        	 InetAddress addr;
+			addr = InetAddress.getByName("localhost");
+			client=new Socket(addr,12345);
+		    sin=new BufferedReader(new InputStreamReader(client.getInputStream()));
+     	    sout=new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())),true);
+  
+            setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+            setLocationByPlatform(true);
+            Image img=new ImageIcon("icon.jpg").getImage();
+            setIconImage(img);
+		   // setLayout(new GridLayout(2,2)); 
+		    
+          chatSpace=new JTextArea();
+          chatSpace.setLineWrap(true);
+          chatSpace.setEditable(false);
+          chatSpace.setBackground(new Color(44,0,30));
+          Font sansbold14=new Font("SansSerif",Font.BOLD,14);
+          chatSpace.setFont(sansbold14);
+          chatSpace.setForeground(Color.white);
+          JScrollPane chatScroll=new JScrollPane(chatSpace);
+          
+          userSpace=new JTextArea();
+          userSpace.setLineWrap(true);
+          userSpace.setEditable(false);
+         // userSpace.setBackground(Color.lightGray);
+          userSpace.setBackground(new Color(44,0,30));
+          Font sansbold10=new Font("SansSerif",Font.BOLD,11);
+          userSpace.setFont(sansbold10);
+          userSpace.setForeground(Color.white);
+         JScrollPane userScroll=new JScrollPane(userSpace);
+         
+          composeMessageSpace=new JTextArea(5,29);
+          composeMessageSpace.setLineWrap(true);
+          Font sansbold12=new Font("SansSerif",Font.BOLD,11);
+          composeMessageSpace.setFont(sansbold12);
+          composeMessageSpace.setForeground(Color.white);
+         // composeMessageSpace.setBackground(Color.lightGray);
+          composeMessageSpace.setBackground(new Color(44,0,30));
+          JScrollPane enterScroll=new JScrollPane(composeMessageSpace);
+          
+          sendButton=new JButton("Send");
+          sendButton.addActionListener(new SendAction());
+          quitButton=new JButton("Quit");
+          quitButton.addActionListener(new QuitAction());
+          loginButton=new JButton("Login");
+          loginButton.addActionListener(new LoginAction());
+          buttonPanel=new JPanel(new GridLayout(3,1));
+          buttonPanel.add(sendButton);
+          buttonPanel.add(quitButton);
+          buttonPanel.add(loginButton);
+          
+       
+          
+          composeMessageSpacePanel=new JPanel();
+          composeMessageSpacePanel.add(enterScroll);
+        composeMessageSpacePanel.add(buttonPanel);
+
+         // getContentPane().add(buttonPanel);   
+         add(chatScroll,BorderLayout.CENTER);
+         add(userScroll,BorderLayout.EAST);
+         add(composeMessageSpacePanel,BorderLayout.SOUTH);
+        // add(buttonPanel,BorderLayout.EAST);
+      }
+	    public void unlogined(String str) throws IOException
+		{   //when unlogin,only these message can be display,contains the quit.
+			 if(str.equals("please login")||str.equals("Name exist, please choose anthoer name.")||str.equals("Invalid command")||(!loginornot&&str.equals("/quit")))
+	          { if(!(str.equals("/quit"))){
+                chatSpace.append(str+"\n");}
+	             else{                                         //when status=unlogin,quit->close socket 
+	            	 chatSpace.append("you are quit"+"\n");sin=null;sout=null;
+	    	          if(ClientThread.client!=null)
+	                //  ClientThread.client.close();
+	                  return;
+	    	          }}
+		}
+		public void logined(String str) throws IOException
+		{
+			if(!(str.equals("OK"))&&!(str.contains("/quit")))
+            {                     //do not dsiplay these command,just give them to server to handle.
+             String temp1=str.substring(0,3);
+             String temp2=str.substring(3);
+             if(str.equals("mlp")){userSpace.setText("");}
+             else  if(temp1.equals("plm")){userSpace.append(temp2+"\n");}
+             else  chatSpace.append(str+"\n");}     
+           else if(str.contains("/quit"))
+           {                  //status=logined,quit
+              chatSpace.append("you are quit"+"\n");
+               if(ClientThread.client!=null)
+            	  // ClientThread.client.close();
+               return;                }	
+		}
+	//rivate class  ThreadClient  implements Runnable {
+			
+			  public void run()
+		        {try { 
+				     while(true){
+				       String str=null;
+				       str=sin.readLine(); 
+				       if(str.equals("/quit"))sin=null;
+				       unlogined(str);
+				       if(str.equals("OK"))                                
+				           { loginornot=true;sout.println("/who");}
+				       if(loginornot)
+				            {    logined(str); }
+			           }}catch(IOException e)
+			               {e.printStackTrace();
+			               }
+		               finally{
+		            	   try{
+		            		   if(ClientThread.client!=null)
+		            			   ClientThread.client.close(); return;
+		            	         }catch(Exception e){e.printStackTrace();}
+		                   }
+		            }
+	  
+      
+      private class SendAction implements ActionListener{
+        public void actionPerformed(ActionEvent arg){
+        	try{
+          String message=composeMessageSpace.getText().trim();
+          if(message!=null){sout.println(message);}
+                               }catch(Exception e){
+                            	   e.printStackTrace();System.out.println("Something is wrong in client-WriteMessage");}
+        	composeMessageSpace.setText("");
+        }}
+      private class LoginAction implements ActionListener{
+          public void actionPerformed(ActionEvent arg){
+        	  String myname=composeMessageSpace.getText().trim();
+              sout.println("/login "+myname);
+              composeMessageSpace.setText("");
+       
+          }}
+      private class QuitAction implements ActionListener{
+          public void actionPerformed(ActionEvent arg){
+        	   sout.println("/quit");
+          }}
+	    public static void main(String[] args)
+            { 
+	    	EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					ClientThread c = null;
+					try {c = new ClientThread();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					c.setTitle("chatroom");
+					c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					
+					c.setVisible(true);
+					Thread thread=new Thread(c) ;
+					thread.start();
+					
+				}
+			});	}}
+           
--- ThreadServer.java
+++ ThreadServer.java
@@ -9,43 +9,36 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-//服务器端线程
-//多线程方面的改进：加了一些volatile修饰，CopyorWriteArrayList的使用
-//                 使用了lock（）和unlock()；主要是防止用户名单之间信息的修改不同步造成异常。
-//用户信息存储在两个地方clients和users。各有用途吧，也没有很大必要去统一。
-//为防止异常每个try后都有catch and finally部分，或者开始就throws.
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-class ServerThreadCode extends Thread
-{   //Server线程，1.从Clien收到后处理login/quit，broadcast,personal，command
-	//2.聊天记录list 3.用户列表map
-	 boolean a=false;     //用来标识是否登录成功的flag,登录后a=true;
+class ServerThreadCode  extends Thread
+{   //Server，1.handling login/quit，broadcast,personal，command
+	//2.chat history :list 3.user-list:map
+	 boolean a=false;     //when status=logined,a=true;
 	private Socket clientSocket;
 	private PrintWriter sout;
 	private BufferedReader sin;
 	CopyOnWriteArrayList<Socket> clients;                                     
-	//socket链表，用来遍历群发
+	public static Lock lock=new java.util.concurrent.locks.ReentrantLock(); //Thread Lock
 	private String name;
 	private static HashMap<String,Socket> users = new HashMap<String,Socket>();
-	//map用来存储每个连接的客户端的用户名和socket
+	//map:user-name user-Socket
 	public ServerThreadCode(Socket client,CopyOnWriteArrayList<Socket> socketList)throws IOException
 	{   name=null;
 		this.clientSocket=client;
 		this.clients=socketList;
 	}
+	@SuppressWarnings("unchecked")
 	public  void history(String str,String str1,String str2)
-	{       //记录聊天记录的函数
+	{     //history
 		ThreadServer.jilu.get(0).add(str);
         ThreadServer.jilu.get(1).add(str1);
         ThreadServer.jilu.get(2).add(str2);
       }
 	public  void qunfa(CopyOnWriteArrayList<Socket> clients,Socket clientSocket,String str)
-	{ //群发函数，除了自己
+	{ //qunfa
 		for(Socket socket:clients) {                            
              if(clientSocket!=socket){            
 		           PrintWriter pw = null;
@@ -54,103 +47,106 @@ class ServerThreadCode extends Thread
 					e.printStackTrace();} 
    	           pw.println(str);}    }	
 	 }
-	public  void delete()
-	{
-		
-	}
 	public void dupname()
 	{   
 		boolean dupornot=false;;
-	    ThreadServer.lock.lock(); 
+	    lock.lock(); 
+      try{
         Iterator <String> iterator =users.keySet().iterator();//提取map的key放到keyset集合，并实例化一个keyset的集合
         while(iterator.hasNext()){                       
 	        String key=iterator.next();
 	         if(name.equals(key))   {
 	            sout.println("Name exist, please choose anthoer name.");
 	           dupornot=true;    }	    				    			
-                                                          }
-        ThreadServer.lock.unlock();						
+        } }
+      finally{lock.unlock();	}					
         if(!dupornot){
 	          users.put(name,clientSocket);a=true;
 	          sout.println("OK");
-	          sout.println("You have logined");
+	          sout.println("You have logined,your name is "+name);
 	          history("You have logined", name, "true");
-	          ThreadServer.lock.lock();
+	          lock.lock();
+            try{
 		      qunfa(clients,clientSocket,name+" has logined ");
-		      history(name+" has logined", name,"false");
-		      ThreadServer.lock.unlock();	  		
+		      history(name+" has logined", name,"false");}
+          finally{lock.unlock();	}  		
 	                 }    
 	}
 	public void deletesocket()
 	{
-		 ThreadServer.lock.lock();  //从socket表中删除,写成函数
+		 lock.lock();  //delete user frome socket list
+     try{
 	       for(int i=0;i<clients.size();i++) {   
 			   if(clientSocket.equals(clients.get(i))) {     
 				   clients.remove(i);               }  
-		   }                     
-	       ThreadServer.lock.unlock();
-	}
+		   } }
+     finally{lock.unlock();
+	}}
 	public void deletemap()
 	{
-		 ThreadServer.lock.lock();         //删除,从users和list中都删除该用户,写成函数
-	       Iterator <String> iterator =users.keySet().iterator();
+		 lock.lock();         //delete user form user and lists
+	   try{  
+     Iterator <String> iterator =users.keySet().iterator();
 		   while(iterator.hasNext())  {
 			    String key=iterator.next();
 			    if(name.equals(key)){
-			             iterator.remove(); } }	
-		   ThreadServer.lock.unlock();	    	
+			             iterator.remove(); } }	}
+     finally{lock.unlock();}  	
 	}
 	public void closesocket() throws IOException
 	{
-		   clientSocket.close();sin.close();sout.close();  //关闭线程并置为null
+		   clientSocket.close();sin.close();sout.close();  //close the Thread and set it=null
 		   clientSocket=null;sin=null;sout=null; return;
 	}
 	public void quit()
 	{
-		 ThreadServer.lock.lock();   
+		 lock.lock();   
+     try{
 		   qunfa(clients, clientSocket, name+" has quit");
-		   history(name+" has quit ",name,"false");
-		   ThreadServer.lock.unlock();	  
+		   history(name+" has quit ",name,"false");}
+     finally{lock.unlock();}
 	       sout.println("/quit");
 	       a=false; 
 	}
    public void yushe(String str1,String str2)
-	    { ThreadServer.lock.lock();
+	    { lock.lock();
+        try{
 		  qunfa(clients, clientSocket, str1);
-          history(str1,name,"false");
-		  ThreadServer.lock.unlock();		    					 		    					 
+          history(str1,name,"false");}
+        finally{lock.unlock();	}   					 		    					 
 		  sout.println(str2); 
           history(str2, name, "true");
 	    }
    public void secert(String temp) throws IOException
    {
-	   ThreadServer.lock.lock();
+	   lock.lock();
+     boolean cunzai=false;
+     try{
 	   Iterator <String> iterator =users.keySet().iterator();
-	   boolean cunzai=false;
 	   while(iterator.hasNext()){
 			String key=iterator.next();
 			if(temp.contains(key)){
 			     cunzai=true;
 			     if(!(key.equals(name))){
-			          Socket value=(Socket)users.get(key);
+			          Socket value=users.get(key);
 			          PrintWriter pw1 = new PrintWriter(value.getOutputStream(),true);
 			          temp=temp.replace(key,"");
-			          String siliao1=name+"对你说 ："+temp;
-			          String siliao2="你对"+key+"说："+temp;
+			          String siliao1=name+"said to you："+temp;
+			          String siliao2="you said to"+key+"："+temp;
 			          pw1.println(siliao1);	 history(siliao1, key, "true");
 			          sout.println(siliao2);history(siliao2, name, "true");
 			                            }	
 		      else{
 		    	sout.println("Stop talking to yourself!");}            
-			                      }	    				    			
-		     }ThreadServer.lock.unlock();
-	if(!cunzai){
-		sout.println("user_name is not online.");}		    		
+			                      }}}
+     finally{lock.unlock();}
+	if(!cunzai){sout.println("user_name is not online.");	}    		
    }
    public void hisomeone(String temp) throws IOException
    {
 	   boolean qunfa=true;
-	   ThreadServer.lock.lock();
+	   lock.lock();
+     try{
 	   Iterator <String> iterator =users.keySet().iterator();
 	   while(iterator.hasNext()){
 			String key=iterator.next();
@@ -158,34 +154,36 @@ class ServerThreadCode extends Thread
 				@SuppressWarnings("resource")
 				Socket sockettmp=new Socket();
 			    sockettmp=users.get(key);
-			    String hi1=name+"向"+key+"打招呼，“Hi，你好啊~”",hi2="你向"+key+"打招呼 ：“Hi,你好啊~”";
+			    String hi1=name+" said to "+key+" say hi",hi2=" you said to "+key+" say hi";
 				for(Socket socket:clients){  
                        if((clientSocket!=socket)&&(socket!=sockettmp)){            
   				          PrintWriter pw = new PrintWriter(socket.getOutputStream(), true); 
   				          pw.println(hi1);}      
                        if(socket==sockettmp){
                       	  PrintWriter pw = new PrintWriter(socket.getOutputStream(), true); 
-  				          pw.println(name+"向你打招呼，“Hi，你好啊~”");}
+  				          pw.println(name+"say hi to you");}
                        }	  history(hi1, name, "false");   				    				
 		    	    sout.println(hi2);history(hi2, name, "true");     
 				qunfa=false;	    				    			
 		                          }		    				    		
-	                            }	ThreadServer.lock.unlock();
+	                            }	}
+     finally{lock.unlock();}
 	  if(qunfa){
-		         String hi3=name+"向大家打招呼：“Hi，大家好！我来咯~”",hi4="你向大家打招呼 :“Hi，大家好！我来咯~”";
-		         ThreadServer.lock.lock();
-                qunfa(clients, clientSocket,hi3);
-			    ThreadServer.lock.unlock();
+		         String hi3=name+"say hi to everyone",hi4="you say hi to everyone";
+		         lock.lock();
+             try{
+                qunfa(clients, clientSocket,hi3);}
+             finally{lock.unlock();}
 		        sout.println(hi4);
 		        history(hi3,name,"false");history(hi4, name, "true");
 		       }
    }
    public void history(String content)
    {
-	   int k=ThreadServer.jilu.get(0).size()-1;int count=0;//k：总的消息数目，count：用户需要显示消息数目
+	   int k=ThreadServer.jilu.get(0).size()-1;int count=0;//k：the sum-value of message，count：the value of user need
        for(int j=0;j<=k;j++){if(((ThreadServer.jilu.get(1)).get(j)==name&&(ThreadServer.jilu.get(2).get(j)=="true"))||(((ThreadServer.jilu.get(1)).get(j)!=name)&&((ThreadServer.jilu.get(2)).get(j)=="false")))
-       	                    count++;}   //if中判断出对于本用户来说需要显示的消息记录是哪些
-   	if(content.equals("/history"))      //无参，最多五十条
+       	                    count++;}   //if:get the message which the user need
+   	if(content.equals("/history"))      //without value,MAX=50
             {   for(int j=0;j<=k;j++){
            	 if(((ThreadServer.jilu.get(1)).get(j)==name&&(ThreadServer.jilu.get(2).get(j)=="true"))||(((ThreadServer.jilu.get(1)).get(j)!=name)&&((ThreadServer.jilu.get(2)).get(j)=="false"))){
            	   if(count<=50){sout.println(count+":"+ThreadServer.jilu.get(0).get(j));}	count--;   }     
@@ -198,11 +196,11 @@ class ServerThreadCode extends Thread
               while(m.find())              //尝试在目标字符串中查找下一个匹配子串
                { d[i++]=Integer.parseInt(m.group()); 
                  if(i==2)break;}            //返回查找获得的所有子串内容,并防止越界
-              int tmp,tmp1;                 //几个式子，从本用户需要显示消息的第tmp1到tmp。。。
+              int tmp,tmp1;                 //this user need display the tmp1-st to tmp-st
               if(d[0]>count)tmp=1;else tmp=count+1-d[0];
               if((d[0]+d[1])>count+1)tmp1=1;else tmp1=count-d[0]-d[1]+2;
-              int counttmp=0;              //counttmp计数，找到编号为从tmp1到tmp的消息。
-              int counttmp2=tmp-tmp1+1;    //显示条目编号，总共这么多条
+              int counttmp=0;              //counttmp，find the message form tmp1 to tmp。
+              int counttmp2=tmp-tmp1+1;    
               for(int j=0;j<=k;j++)
                {   
            	   if(((ThreadServer.jilu.get(1)).get(j)==name&&(ThreadServer.jilu.get(2).get(j)=="true"))
@@ -212,17 +210,25 @@ class ServerThreadCode extends Thread
             }  }						               
                                 
    }
-   public void who()
+   @SuppressWarnings("unused")
+public void who()
    {
-	   int count=0;               ThreadServer.lock.lock();
+	   
+	int count=0;               
+     lock.lock();
+     try{
 	    Iterator <String> iterator =users.keySet().iterator();
+	    qunfa(clients,null,"mlp");
 	    while(iterator.hasNext()){
 		      String key=iterator.next();
-		      sout.println(key);count++;
-	                             }	ThreadServer.lock.unlock();	    				    		
-	    sout.println("Total online user: "+count );		    			
+		      //sout.println("plm"+"++++"+key);
+		      qunfa(clients,null,"plm"+"++++"+key);
+		      count++;
+	                             }}
+     finally{lock.unlock();	}    				    		
+	    //sout.println("Welcome!Now there are "+count+" users online" );		    			
    }
-   public void run()//完成大部分任务，login/quit，command，broadcast，personal
+   public void run()//login/quit，command，broadcast，personal
 	{ try {
 		    sout=new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())),true);
 		    sin=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));		    
@@ -231,43 +237,46 @@ class ServerThreadCode extends Thread
 		    while(true){
 		    	  if(!a){
 		    		     if((content=sin.readLine()).equals("/quit"))            
-		    		    	 //未登录就退出，close线程，return
+		    		    	 //status=unlogin,close the Thread
 					          {      sout.println("/quit");
 					                 deletesocket();
-					                  closesocket(); }
+					                  closesocket(); 
+					                  }
 		    	        else if(content.contains("/login "))                    
-		    	        	//登录：获取姓名，包括检查重名，以及通知其他客户端登陆
+		    	        	
 		    	              {    name=content.substring(7);
 					                dupname();				 
-					                 //先检测是否重名,若无重名则加入到列表中
+					                 //no dupname
 					                  }
 		    		    else{sout.println("Invalid command");   }		      
-		    		     //无效命令    		
+		    		     
 		    	             }
-		         if(a){                //登录之后：退出，预设消息，普通消息，私聊，命令/map,/history [a1],[a2];
+		         if(a){                //status=logined：/quit，normal message，sercert，command/map,/history [a1],[a2];
 		    		    if((content=sin.readLine()).equals("/quit"))
 		    		        {    quit();
 		    			         deletemap();
 		    			         deletesocket();
 		    			         closesocket();
-	                             return;  }		    		
-		    		    else{          //非退出命令即进入
+		    			        who();
+	                             return;  }	
+		    		    else if(content.contains("/login ")){}
+		    		    else{          
 		    				   if(content.equals("//smile")){               
                                         //预设消息//smile
-		    					      String yushe1=name+" 的脸上浮起微笑";
-		    					      String yushe2="你的脸上浮起微笑";
+		    					      String yushe1=name+" smiled";
+		    					      String yushe2="you smiled";
 		    					      yushe(yushe1,yushe2);}
 		    				   else if(content.equals("//hehe")){            
 		    					      //预设消息//hehe
-		    					      String yushe1=name+"表示呵呵";
-		    					      String yushe2="你表示呵呵";
+		    					      String yushe1=name+"said hehe";
+		    					      String yushe2="you said hehe";
                                        yushe(yushe1,yushe2);}
 		    			        else if(content.contains("/to"))            
-		    			        	 //私聊
+		    			        	 //sercert
 		    				       {   String temp=content.substring(4);
 		    				            secert(temp);   }	
 		    				    else if(content.contains("//hi")){       
-		    				    	 //预设消息 //hi [somebody],写成函数
+		    				    	 //预设消息 //hi [somebody]
 		    				    	   String temp=content.substring(4);
 		    				    	   hisomeone(temp);   }	
 		    				    else if(content.contains("/history")){  
@@ -275,38 +284,39 @@ class ServerThreadCode extends Thread
 		    				            history(temp);	  }					 	    
 		    				    else if(content.equals("/who")){      
 		    				    	   	    	who();     }		    				    
-		    				    else{   ThreadServer.lock.lock();
-		    				    	          qunfa(clients, clientSocket, name+"说："+content);
-		    				    	          ThreadServer.lock.unlock();
-		    				    	          sout.println("你说： "+content);
-		    				    	          history(name+"说："+content, name, "false");
-		    				    	          history("你说： "+content, name, "true");	}	    			      
+		    				    else{   lock.lock();
+                            try{
+		    				    	          qunfa(clients, clientSocket, name+"said："+content);}
+                            finally{
+		    				    	          lock.unlock();}
+		    				    	          sout.println("you said： "+content);
+		    				    	          history(name+"said："+content, name, "false");
+		    				    	          history("you said： "+content, name, "true");	}	    			      
 		    			      }  }
 		    		  }}catch(IOException e)
 		                      {e.printStackTrace();
 		                      System.out.println("Something is wrong in server");}
-		                finally{System.out.println("Close the Server socket and the io.");//提示关闭服务器并关闭线程
+		                finally{System.out.println("Close the Server socket and the io.");
 			                     try{if(clientSocket!=null)clientSocket.close();
-			                         if(sin!=null)sin.close();
-			                         if(sout!=null)sout.close();}catch(IOException e)
+			                         if(sin!=null)sin=null;
+			                         if(sout!=null)sout=null;}catch(IOException e)
 			                                                    {e.printStackTrace();}
 		                       }} }
 
 public class ThreadServer
 {   
-	public static Lock lock=new java.util.concurrent.locks.ReentrantLock(); //线程锁
 	public static ArrayList<String> xiaoxi=new ArrayList<String>(),shuyu=new ArrayList<String>(),flag=new ArrayList<String>();;       
 	@SuppressWarnings("rawtypes")
 	public static ArrayList<ArrayList> jilu=new ArrayList<ArrayList>();    //归属用两个标志，shuyu和flag.其中name---0为除name外广播，name---1为私有。       
 	public static final int portNo=12345;       
 	public volatile static  CopyOnWriteArrayList<Socket> socketList=new CopyOnWriteArrayList<Socket>();
 	public static void main(String[]args)throws IOException
-	{   jilu.add(xiaoxi);jilu.add(shuyu);jilu.add(flag);                 //历史消息list
+	{   jilu.add(xiaoxi);jilu.add(shuyu);jilu.add(flag);                 //message history :list
 		ServerSocket serversocket=new ServerSocket(portNo);
-		System.out.println("The Server is start:"+serversocket);         //正常启动提示
+		System.out.println("The Server is start:"+serversocket);         //start success
 		try {while(true)
-			   {  Socket socket=serversocket.accept();                   //阻塞，等待客户端的到来
-				  socketList.add(socket);                                //将这个线程存储起来便于后面群发
+			   {  Socket socket=serversocket.accept();                   //wait client
+				  socketList.add(socket);                                //save this Thread
 				  new ServerThreadCode(socket,socketList).start();//run();
 			   }
 		    }catch(IOException e){e.printStackTrace();}
